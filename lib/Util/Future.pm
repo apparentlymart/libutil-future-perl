f2189423a3760a4f5246b87940e5a1e1b15d2157
Martin Atkins <mart@degeneration.co.uk>
2009-09-03 00:46:08 -0700

    Initial import.

---
+++ lib/Util/Future.pm
@@ -0,0 +1,313 @@
+
+=head1 NAME
+
+Util::Future - A description of something to load in future
+
+=cut
+
+package Util::Future;
+
+use strict;
+use warnings;
+use Scalar::Util qw(refaddr blessed);
+use Carp;
+use Util::FutureManager;
+
+use constant ON_SATISFY_CALLBACKS => __PACKAGE__.'::on_satisfy_callbacks';
+use constant RESULT => __PACKAGE__.'::result';
+
+### METHODS FOR SUBCLASSES TO OVERRIDE
+
+sub handler_class {
+    # Default is the class our instance is blessed onto
+    return blessed($_[0]);
+}
+
+sub batching_key {
+    return 'all';
+}
+
+sub instance_key {
+    # Default is our instance refaddr, which makes each
+    # instance completely distinct. This is a safe default,
+    # but subclasses really should override this to return
+    # a true coalescing key to avoid duplicate lookups.
+    return refaddr($_[0]);
+}
+
+sub satisfy_multi {
+    croak "$_[0] does not provide an implementation of satisfy_multi";
+}
+
+### METHODS FOR SUBCLASSES TO CALL
+
+sub satisfy {
+    my ($self, $result) = @_;
+
+    croak "Can't re-satisfy $self" if exists $self->{RESULT()};
+    $self->{RESULT()} = $result;
+
+    Util::FutureManager->register_satisfaction($self);
+    $self->run_on_satisfy_callbacks();
+}
+
+sub inject {
+    my ($self) = @_;
+
+    return Util::FutureManager->ensure_future_in_queue($self);
+}
+
+### PUBLIC INSTANCE METHODS
+
+sub result {
+    my ($self) = @_;
+
+    croak("$self is not yet satisfied") unless exists $self->{RESULT()};
+
+    return $self->{RESULT()};
+}
+
+sub satisfied {
+    return exists($_[0]->{RESULT()}) ? 1 : 0;
+}
+
+### PUBLIC CLASS METHODS
+
+sub create_sequence {
+    my ($class, $future, $cb) = @_;
+
+    my $callback = sub {
+        local $_ = $_[0];
+        $cb->($_);
+    };
+
+    # If the future's already been satisfied (i.e. it came out of the
+    # satisfied cache) then we can just run the callback right away
+    # and find out what else we need to load without doing
+    # an extra iteration.
+    if ($future->satisfied) {
+        $cb->($future->result);
+        return $future;
+    }
+    else {
+        $future->add_on_satisfy_callback(sub {
+            local $_ = $_[0];
+            $cb->($_);
+        });
+        return $future;
+    }
+}
+
+### PRIVATE METHODS
+
+sub add_on_satisfy_callback {
+    my ($self, $cb) = @_;
+
+    croak "Provided callback must be a CODE reference" unless ref($cb) eq 'CODE';
+
+    $self->{ON_SATISFY_CALLBACKS()} ||= [];
+    push @{$self->{ON_SATISFY_CALLBACKS()}}, $cb;
+
+    return undef;
+}
+
+sub run_on_satisfy_callbacks {
+    my ($self) = @_;
+
+    my $result = $self->result;
+    foreach my $cb (@{$self->{ON_SATISFY_CALLBACKS()}}) {
+        $cb->($result);
+    }
+}
+
+1;
+
+=head1 SYNOPSIS
+
+    package My::App::Future::LoadUser;
+    
+    use base qw(Util::Future);
+    
+    sub inject_for_user_id {
+        my ($class, $user_id) = @_;
+        
+        # Object MUST be a blessed HASH ref.
+        my $self = bless {}, $class;
+        
+        $self->{user_id} = $user_id;
+        
+        # You MUST return what $self->inject returns,
+        # not $self directly. (See SUBCLASS API below
+        # to find out why.)
+        return $self->inject();
+    }
+
+=head1 DESCRIPTION
+
+This is an abstract base class for different classes of
+'future'. A 'future' is a description of something
+you want to load in future, but you don't want to load
+right now.
+
+L<Util::FutureManager> can then be used to load
+all queued futures in batch, with as few I/O round
+trips as possible.
+
+=head1 PUBLIC INSTANCE API
+
+=head2 $future->result
+
+Returns whatever value resulted from executing the
+load operation associated with this future, or dies
+if the load has not yet taken place.
+
+=head2 $future->add_on_satisfy_callback($cb)
+
+Adds a callback which will be called once this
+future's result has been delivered. See
+the C<create_sequence> class method below
+for a convenient wrapper around this.
+
+=head1 PUBLIC STATIC API
+
+The following utility methods are provided as
+shorthands for operations that would normally
+require multiple steps.
+
+=head2 Util::Future->create_sequence($future, $callback)
+
+Attaches to $future a callback that will
+put the result in a localized C<$_> and the call your
+provided callback once the future is satisfied.
+
+Returns the provided future with the callback attached.
+
+This is intended to allow you to write calls like:
+
+    Util::Future->create_sequence(
+        My::App::Future::User::Load->inject_for_user_id($user_id),
+        sub {
+            My::App::Future::User::LoadUserpic->inject_for_user($_);
+        }
+    );
+
+Calls to C<create_sequence> can be nested to create
+multi-step loading sequences.
+
+=head1 SUBCLASS API
+
+=head2 $class->satisfy_multi($futures, $batching_key)
+
+Called by L<Util::FutureManager> to satisfy a bunch of
+futures of a given type.
+
+C<$futures> is a HASH ref mapping instance_id to future
+instance. All of the futures provided are guaranteed to
+have a C<batching_key> matching that in C<$batching_key>.
+
+Any subclass that's ever returned as the C<handler_class>
+for a future must override this method.
+
+The implementation of C<satisfy_multi> B<MUST> call
+C<$future-E<gt>satisfy($value)> exactly once on every future in the
+provided set before returning. You may explicitly satisfy with C<undef> if
+there is no useful result to return for a particular
+future.
+
+=head2 $future->satisfy($result)
+
+To be called by your C<satisfy_multi> implementation once
+you've got a result for a particular future.
+
+This must only be called from a C<satisfy_multi> implementation.
+Calling it elsewhere will cause things to get into an
+inconsistent state, so don't do that.
+
+=head2 $future->handler_class
+
+Called to determine which class we will invoke C<satisfy_multi>
+on to satisfy this class.
+
+The default implementation returns the class onto which C<$future>
+is blessed, but subclasses can override this if they wish
+to handle several different kinds of futures with a single
+base class.
+
+=head2 $future->batching_key
+
+Called to determine which batch this future will be placed
+into when loading.
+
+This allows multiple futures of the same type to
+actually be satisfied via separate calls to C<satisfy_multi>.
+This can be useful, for example, if your handler
+loads some data about a bunch of users but your user
+data is partitioned into separate data stores and you
+can only query one partition at a time: just
+set batching_key to be the partition id and you'll
+automatically get a separate C<satisfy_multi> call for each
+partition.
+
+The default implementation returns the literal string
+"all", which causes all futures to get handled together
+in a single batch.
+
+Subclasses can override this to return a batch discriminator
+if they need to distinguish between different batch
+loads of the same type, but the C<batching_key> MUST be
+a string or something that can sensibly be compared as a string
+since it will be used as a hash key internally.
+
+=head2 $future->instance_key
+
+Called to determine a unique key for this specific operation.
+Multiple futures with the same (C<handler_class>, C<batching_key>,
+C<instance_key>) tuple will be coalesced together and
+handled as a single future.
+
+The default is to return the C<refaddr> of the $future instance,
+which effectively makes each future distinct. It is strongly
+recommended for subclasses to override this and return a true
+identifier for what is being loaded, such as the primary key
+of the object if your instance is loading data relating to
+a particular object from a data store.
+
+Two futures with the same batching tuple should NEVER exist
+concurrently in memory except within the scope of an
+injector method as described in the following section.
+
+=head2 Your own injector method(s)
+
+Subclasses must provide one or more class methods to create and
+inject new futures of that type. These MUST be of the following
+form:
+
+    sub inject_for_user_id {
+        my ($class, $user_id) = @_;
+
+        # $self MUST be a blessed HASH ref
+        my $self = bless {}, $class;
+
+        $self->{user_id} = $user_id;
+
+        # You MUST return what $self->inject returns
+        return $self->inject;
+    }
+
+There are two important requirements here.
+
+First, you MUST use a HASH ref as the backing store for your
+instances. This is because the base class will write its own
+state in there and expects to be able to treat C<$self> as a
+hash.
+
+Secondly, you MUST end your injector method with a call
+to C<$self->inject> AND return what it returns. DO NOT
+return your own C<$self> directly under any circumstances.
+
+This is because internally L<Util::FutureManager> ensures
+that no two instances of the same task (identified by
+its coalescing tuple as described above) exist, and
+so the return value of C<inject> may be a different instance
+of the same class that another caller injected earlier.
+
---
+++ lib/Util/FutureManager.pm
@@ -0,0 +1,249 @@
+
+=head1 NAME
+
+Util::FutureManager - Manager of queues of futures
+
+=cut
+
+package Util::FutureManager;
+
+use strict;
+use warnings;
+use Scalar::Util qw(blessed);
+
+### CONFIGURATION
+# This stuff is supposed to be set on application startup.
+
+# Where a caller has requested that a particular future class
+# be loaded before another they end up getting weights
+# in here which we use to decide which order to run
+# batches in. Otherwise, they just run in an undefined order.
+our %Class_Weights = ();
+
+### GLOBAL STATE
+# This stuff is generally global, but a with_local_future_queue
+# block localizes this state to temporarily create a separate
+# context.
+
+# The items that still need to be loaded, if any.
+# Structure is future_class => batching_key => instance_key => future
+our %Queue = ();
+
+# We keep the queue size as an integer so that we
+# can quickly determine if there's anything left to
+# process
+our $Queue_Size = 0;
+
+### STATE DURING LOADING
+# These variables get localized during a load operation
+# to retain state that is global during that operation.
+
+# Called when $class->register_statisfaction is called
+# so that the currently-running batch loop can update
+# its lexical state.
+our $Satisfaction_Callback = sub {};
+
+# Called when a new future is injected so that the
+# currently-running batch loop can update its lexical
+# state.
+our $Injection_Callback = sub {};
+
+### STATE DURING satisfy_futures
+# These are localized for the whole of satisfy_futures
+
+# This retains references to all future objects that
+# have been satisfied so that later instances of
+# the same future can return immediately without
+# a load.
+our %Satisfied_Cache = ();
+
+# Need to predeclare this so code below can see the prototype
+sub with_profiler(&@);
+
+### CLASS METHODS
+
+# Public Interface
+
+sub satisfy_futures {
+    my ($class) = @_;
+
+    # Fast Path
+    return unless $Queue_Size > 0;
+
+    # Where we keep loaded objects so we don't load
+    # the same things multiple times.
+    local %Satisfied_Cache = ();
+
+    my $future_classes = $class->future_classes_in_queue;
+
+    while ($Queue_Size > 0) {
+        my $satisfied_this_iteration = 0;
+        my $satisfied_this_load = 0;
+
+        local $Satisfaction_Callback = sub {
+            $satisfied_this_iteration++;
+            $satisfied_this_load++;
+        };
+
+        foreach my $handler_class (@$future_classes) {
+            foreach my $batching_key (keys %{$Queue{$handler_class}}) {
+                my $futures = $Queue{$handler_class}{$batching_key};
+
+                my $expected_to_satisfy = scalar(keys(%$futures)) + 0;
+                next unless $expected_to_satisfy > 0;
+
+                $satisfied_this_load = 0;
+
+                Util::FutureManager::with_profiler {
+                    # Actually satisfy all of the futures
+                    $handler_class->satisfy_multi($futures, $batching_key);
+                } $handler_class, $batching_key, $expected_to_satisfy;
+
+                if ($satisfied_this_load != $expected_to_satisfy) {
+                    die "$handler_class $batching_key batch expected to satisfy $expected_to_satisfy but actually satisfied $satisfied_this_load";
+                }
+            }
+        }
+
+        die "Did an iteration without satisfying anything" unless $satisfied_this_iteration > 0;
+    }
+
+    return undef;
+}
+
+sub future_classes_in_queue {
+    my ($class) = @_;
+
+    return [ sort { $Class_Weights{$a} <=> $Class_Weights{$b} } keys %Queue ];
+}
+
+sub queue_size {
+    return $Queue_Size;
+}
+
+sub set_preferred_load_order {
+    my ($class, $handler_class_1, $handler_class_2) = @_;
+
+    my $first_weight = $Class_Weights{$handler_class_1};
+    $first_weight = $Class_Weights{$handler_class_1} = 0 unless defined($first_weight);
+
+    my $second_weight = $Class_Weights{$handler_class_2};
+    $Class_Weights{$handler_class_2} = $first_weight + 1 unless defined($second_weight) && $second_weight > $first_weight;
+
+    return undef;
+}
+
+# Internal Interface for Util::Future to call into
+
+sub ensure_future_in_queue {
+    my ($class, $future) = @_;
+
+    my $handler_class = $future->handler_class;
+    my $batching_key = $future->batching_key;
+    my $instance_key = $future->instance_key;
+
+    # Do we already have a satisfied future in our cache?
+    if (my $satisfied = $Satisfied_Cache{$handler_class}{$batching_key}{$instance_key}) {
+        return $satisfied;
+    }
+
+    # If we've already got this future in the queue,
+    # we return the one we already have so that
+    # all callers end up holding the same instance
+    # as long as the futures have their constructors
+    # written correctly.
+    if (my $existing = $Queue{$handler_class}{$batching_key}{$instance_key}) {
+        return $existing;
+    }
+    else {
+        $Queue_Size++;
+        $Injection_Callback->($future);
+        return $Queue{$handler_class}{$batching_key}{$instance_key} = $future;
+    }
+}
+
+sub register_satisfaction {
+    my ($class, $future) = @_;
+
+    my $handler_class = $future->handler_class;
+    my $batching_key = $future->batching_key;
+    my $instance_key = $future->instance_key;
+    my $old_future = delete($Queue{$handler_class}{$batching_key}{$instance_key});
+    return unless defined($old_future); # Already satisfied?
+
+    $Satisfied_Cache{$handler_class}{$batching_key}{$instance_key} = $future;
+    $Queue_Size--;
+    $Satisfaction_Callback->($future);
+}
+
+# Debugging Interface
+
+my $profiler = sub { $_[0]->() };
+
+sub set_profiler {
+    my ($class, $code) = @_;
+
+    $profiler = $code;
+    return undef;
+}
+
+### PACKAGE FUNCTIONS
+
+sub with_local_future_queue(&) {
+    my ($code) = @_;
+
+    local %Queue = ();
+    local $Queue_Size = 0;
+    local %Class_Weights = ();
+    $code->();
+}
+
+### INTERNAL UTILITY FUNCTIONS
+
+sub with_profiler(&@) {
+    $profiler->(@_);
+}
+
+1;
+
+=head1 METHODS
+
+
+=head1 DEBUGGING/ANALYSIS METHODS
+
+During development it is important to verify that your futures
+are being delivered in the way you expect, and that bugs aren't
+causing dependencies to be handled in a sub-optimal way.
+This class provides a hook through which you can optionally
+track each batch-load to see what individual load calls are
+being made.
+
+=head2 $class->set_profiler($code)
+
+Provide a code ref that will be called when each batch-load
+happens. It is passed the following parameters:
+
+    ($code, $future_class, $batching_key, $count)
+
+Your implementation must start any timer it wishes to start,
+then run C<$code-E<gt>()> (with no arguments) and, once
+it returns, do any logging you want to do.
+
+C<$future_class> and C<$batching_key> are the identifiers
+for this particular batch, while C<$count> is the number
+of distinct futures being handled in this batch.
+
+=head1 FUNCTIONS
+
+=head2 Util::FutureManager::with_local_feature_queue { ... }
+
+Runs the provided block in a context with a localized feature queue.
+
+This is useful if you want to do a small set of segregated loads
+without processing the full queue, but in the common case you'll
+want to do all of your loading in one place for maximum batching
+efficiency.
+
+This also allows you to run a future loop within the loader
+of a future, should you need to do that for some bizarre reason.
+
